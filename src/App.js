66a53cd875961d7609c8fcaa403287cd79c268a5
silent-sea1119 <sandragonzales56t@gmail.com>
2023-02-13 14:33:22 -0800

    Can save color scheme in local storage

--- src/App.js
+++ src/App.js
@@ -1,47 +1,77 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import ColorScheme from "./components/ColorScheme";
 function App() {
-  const [schemeColors, setSchemeColors] = useState([])
+  const [schemeColors, setSchemeColors] = useState([]);
   const [schemeData, setSchemeData] = useState({
     selectedColor: "#FFDB58",
     selectedScheme: "monochrome",
-    numColors: 5
-  })
-  const [mode, setMode] = useState("light")
+    numColors: 5,
+  });
+  const [savedSchemes, setSavedSchemes] = useState(getSavedSchemes());
+  const [mode, setMode] = useState("light");
 
   function handleChange(e) {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
 
     setSchemeData({
       ...schemeData,
       [name]: value,
-    })
+    });
   }
 
   function fetchSchemeColors() {
-    const {selectedColor, selectedScheme, numColors} = schemeData
+    const { selectedColor, selectedScheme, numColors } = schemeData;
     try {
-      fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor.slice(1)}&mode=${selectedScheme}&count=${numColors}`)
+      fetch(
+        `https://www.thecolorapi.com/scheme?hex=${selectedColor.slice(
+          1
+        )}&mode=${selectedScheme}&count=${numColors}`
+      )
         .then((response) => response.json())
-        .then((data) =>setSchemeColors((prevSchemeColors) => data.colors))
+        .then((data) => setSchemeColors((prevSchemeColors) => data.colors));
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
-  useEffect(() => {
-    fetchSchemeColors()
-  }, [schemeData])
+  function handleSaveSchemeClick() {
+    setSavedSchemes((prevSavedSchemes) => [schemeData, ...prevSavedSchemes]);
+  }
 
-  console.log(schemeColors)
+  function getSavedSchemes() {
+    const stringifiedSchemes = localStorage.getItem("savedSchemes");
+    const storedSchemes = JSON.parse(stringifiedSchemes);
+    return storedSchemes || [];
+  }
+
+  useEffect(() => {
+    const stringifiedSchemes = JSON.stringify(savedSchemes);
+    localStorage.setItem("savedSchemes", stringifiedSchemes);
+  }, [savedSchemes]);
+
+  useEffect(() => {
+    fetchSchemeColors();
+  }, [schemeData]);
+
+
   function toggleMode() {
-    mode === "light" ? setMode((prevMode) => "dark") : setMode((prevMode) => "light")
+    mode === "light"
+      ? setMode((prevMode) => "dark")
+      : setMode((prevMode) => "light");
   }
   return (
     <div className={`App ${mode}`}>
-      <Header schemeData={schemeData} handleChange={handleChange} toggleMode={toggleMode} mode={mode}/>
-      <ColorScheme schemeColors={schemeColors}/>
+      <Header
+        schemeData={schemeData}
+        handleChange={handleChange}
+        toggleMode={toggleMode}
+        mode={mode}
+      />
+      <ColorScheme schemeColors={schemeColors} />
+      <button id="save-btn" className="btn save-btn" onClick={handleSaveSchemeClick}>
+        Save Color Scheme
+      </button>
     </div>
   );
 }
