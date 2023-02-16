85967622016d9fb6ca16b61ae542e2de0b73b31e
silent-sea1119 <sandragonzales56t@gmail.com>
2023-02-15 16:34:38 -0800

    Updated button css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,13 @@ function Header({ schemeData, handleChange, toggleMode, mode }) {
     <header className={`header ${mode}`}>
       <h1>Color Scheme Generator</h1>
       <section className="selects">
-        <ColorForm schemeData={schemeData} handleChange={handleChange} mode={mode}/>
-        <button
-          className={`btn btn-${mode} mode-btn`}
-          onClick={toggleMode}
-        >
-         {mode === "light" ? "Dark Mode" : "Light Mode"}
+        <ColorForm
+          schemeData={schemeData}
+          handleChange={handleChange}
+          mode={mode}
+        />
+        <button className={`btn btn-${mode} mode-btn`} onClick={toggleMode}>
+          {mode === "light" ? "Dark Mode" : "Light Mode"}
         </button>
       </section>
     </header>
